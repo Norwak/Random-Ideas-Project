b3538e19aa005296e86047ba03c4ef639f115900
Norwak <norwak99@gmail.com>
2023-11-29 19:13:13 +0500

    Created frontend enviroment, imported a premade template

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
@@ -8,16 +9,16 @@ connectDB();
 const app = express();
 
 
+// Static Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 
 // Body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 
-
-app.get('/', function(request, response) {
-  response.json({message: 'Welcome to the Random Ideas API'});
-});
 
 const ideasRouter = require('./routes/ideas.js');
 app.use('/api/ideas', ideasRouter);
